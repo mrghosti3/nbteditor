331acb1ba965ca456c24a90aae80db71deb9454a
mrghosti3 <mysteriousghst@gmail.com>
2024-01-26 23:56:38 +0200

    squash! Simplify Config struct
    Update test cases.

--- src/test/cli.rs
+++ src/test/cli.rs
@@ -3,12 +3,12 @@ use crate::cli::*;
 #[test]
 fn cli_compile() {
     let args = [
-        "c".to_string().into_boxed_str(),
-        "ghosti3.dat".to_string().into_boxed_str(),
+        Box::from("c"),
+        Box::from("ghosti3.dat"),
     ]
     .into_iter();
     let cli = Config::parse(args).unwrap();
 
     assert_eq!(cli.cmd, Command::Compile);
-    assert_eq!(cli.file_input.as_ref(), "ghosti3.dat");
+    assert_eq!(cli.get_in_file(), "ghosti3.dat");
 }
