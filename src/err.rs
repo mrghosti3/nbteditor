184f65f955e9aaafa8d59d0880b8e555ae12a369
mrghosti3 <mysteriousghst@gmail.com>
2024-01-26 23:30:45 +0200

    Restructure `err` module
    Simplify the module into `ConfigErr` which is returned during setup and
    cli arg processing, `RuntimeErr` for when during other operations fail
    like: OS throws an IO error, unrecoverable NBT library errors and much
    more. Then finally there is `MyError` which wraps up both of above
    mentioned errors for simple processing them before exiting the program
    with non-standart failure code.

--- src/err.rs
+++ src/err.rs
@@ -1,32 +1,11 @@
+use std::io;
 use nbt::decode::TagDecodeError;
 
 #[derive(Debug)]
-pub(crate) enum MyError {
-    BadFilePath,
-    OSError(std::io::Error),
-    YMLError(serde_yaml::Error),
-    NBTError(TagDecodeError),
+pub(crate) enum MyError<'a> {
+    Setup(ConfigErr<'a>),
+    Runtime(RuntimeErr),
 }
-
-impl From<std::io::Error> for MyError {
-    fn from(value: std::io::Error) -> Self {
-        Self::OSError(value)
-    }
-}
-
-impl From<serde_yaml::Error> for MyError {
-    fn from(value: serde_yaml::Error) -> Self {
-        Self::YMLError(value)
-    }
-}
-
-impl From<TagDecodeError> for MyError {
-    fn from(value: TagDecodeError) -> Self {
-        Self::NBTError(value)
-    }
-}
-
-pub(crate) type Result<T> = std::result::Result<T, MyError>;
 
 #[derive(Debug)]
 pub enum ConfigErr<'a> {
@@ -34,3 +13,23 @@ pub enum ConfigErr<'a> {
     BadCommand(&'a str),
     ArgError(&'a str),
 }
+
+#[derive(Debug)]
+pub enum RuntimeErr {
+    OSError(io::Error),
+    NBTError(TagDecodeError),
+}
+
+impl From<io::Error> for RuntimeErr {
+    fn from(value: io::Error) -> Self {
+        Self::OSError(value)
+    }
+}
+
+impl From<TagDecodeError> for RuntimeErr {
+    fn from(value: TagDecodeError) -> Self {
+        Self::NBTError(value)
+    }
+}
+
+pub(crate) type Result<T> = std::result::Result<T, RuntimeErr>;
